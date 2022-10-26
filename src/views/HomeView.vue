9e2ec852fcb4a7e41181ea2dcb4d674c212559b2
Wei Lun Teo <weilun.teo.2021@scis.smu.edu.sg>
2022-10-26 23:18:00 +0800

    update views

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+  <NavigationBar/>
+  <HeroSection/>
+  <FeatureSection/>
+  <Footer/>
+  
+  </template>
+  
+  <script> /*Javascript */
+  import NavigationBar from '../components/landing-page/NavigationBar.vue'
+  import HeroSection from '../components/landing-page/HeroSection.vue'
+  import FeatureSection from '../components/landing-page/FeatureSection.vue';
+  import Footer from '../components/landing-page/Footer.vue';
+  
+  export default {
+  name: "HomeView",
+  components: {
+    NavigationBar,
+    HeroSection,
+    FeatureSection,
+    Footer
+  },
+  data (){
+    return{
+        
+    }
+  },
+  methods:{
+  
+  }
+  
+  }
+  </script>
+  
+  
+  <style> /* CSS*/
+  </style>

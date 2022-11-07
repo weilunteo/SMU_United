aa3e0b7fb87e1d29eeda09f5b628d35d1b0a5c03
weilunteo <105581885+weilunteo@users.noreply.github.com>
2022-11-08 00:05:10 +0800

    Update IndividualSection.vue

--- src/components/individual-page/IndividualSection.vue
+++ src/components/individual-page/IndividualSection.vue
@@ -18,13 +18,13 @@
                 <div id="carouselExampleControls" class="carousel slide box" data-bs-ride="carousel">
                     <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="images/UOL1.jpg" class="d-block w-100" alt="...">
+                        <img src="" class="d-block w-100" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="images/UOL2.png" class="d-block w-100" alt="...">
+                        <img src="" class="d-block w-100" alt="...">
                     </div>
                     <div class="carousel-item">
-                        <img src="images/UOL3.png" class="d-block w-100" alt="...">
+                        <img src="" class="d-block w-100" alt="...">
                     </div>
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">

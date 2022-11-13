d47062718a51a7d02cc7a69eb321e6e0da5abccd
jessicafiore09 <111348094+jessicafiore09@users.noreply.github.com>
2022-11-13 10:13:28 +0800

    revamp landing page - done by jasper

--- src/components/landing-page/NavigationBar.vue
+++ src/components/landing-page/NavigationBar.vue
@@ -1,21 +1,22 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-light py-4">
-  <div class="container-fluid" style="width:100%">
-    <a class="navbar-brand ms-5" href="#">
-      <img src="../../assets/img/smu-united.png" width="0.1" height="0.1">
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <nav class = "navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src = "../../assets/img/thetravellingstudent.png" id = "logo"/>
+      </a>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse " id="navbarSupportedContent">
-      <div class=" navbar-nav ms-auto me-5 mb-2 mb-lg-0" >
-        <router-link to="/login" class="btn btn-primary px-5 mx-2 fw-bold">Login</router-link>
-        <router-link to="/signup"  class="btn btn-outline-primary px-5 mx-2 fw-bold">Sign Up</router-link>
-      </div>
+      
+        <div class="navbar-nav ms-auto me-5 mb-2 mb-lg-0" >
+          <router-link to="/login" class="btn btn-light px-5 mx-2 fw-bold">Login</router-link>
+          <router-link to="/signup"  class="btn btn-outline-light px-5 mx-2 fw-bold">Sign Up</router-link>
+        </div>
+    </div> 
     </div>
-
-  </div>
-</nav>
+  </nav>
 
 </template>
 
@@ -38,14 +39,27 @@ methods:{
 
 
 <style>
+
+.navbar{
+  background-color: #0069fc;
+  position: sticky;
+  top: 0;
+  z-index: 90;
+  box-shadow: rgba(39, 41, 43, 0.2) 0px 8px 24px;
+}
+
+#logo{
+  width: 300px;
+}
 .navbar-brand {
-          display: flex;
-          width: 15%;
-          justify-content: center;
+    display: flex;
+    max-height: 200px;
+    
 }
 .navbar-nav{
   padding-left:30px;
   padding-right:30px;
+  font-family: Inter, 'sans serif';
 }
 
 </style>
